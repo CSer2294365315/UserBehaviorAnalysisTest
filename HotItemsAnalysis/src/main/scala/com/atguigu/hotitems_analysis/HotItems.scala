fe8b872693266ca484fd9e0b3727abc25302dbae
CSer2294365315 <2294365315@qq.com>
2020-02-29 15:39:42 +0800

    增加 就业笔记

--- HotItemsAnalysis/src/main/scala/com/atguigu/hotitems_analysis/HotItems.scala
+++ HotItemsAnalysis/src/main/scala/com/atguigu/hotitems_analysis/HotItems.scala
@@ -19,29 +19,31 @@ import org.apache.flink.util.Collector
 
 import scala.collection.mutable.ListBuffer
 
-/**
-  * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved 
-  *
-  * Project: UserBehaviorAnalysis
-  * Package: com.atguigu.hotitems_analysis
-  * Version: 1.0
-  *
-  * Created by wushengran on 2020/2/24 14:02
-  */
 
 // 定义输入数据样例类
-case class UserBehavior( userId: Long, itemId: Long, categoryId: Int, behavior: String, timestamp: Long )
-// 定义中间聚合结果样例类
-case class ItemViewCount( itemId: Long, windowEnd: Long, count: Long )
+case class UserBehavior(userId: Long, itemId: Long, categoryId: Int, behavior: String, timestamp: Long)
 
+// 定义中间聚合结果样例类
+case class ItemViewCount(itemId: Long, windowEnd: Long, count: Long)
+
+
+//就这三种可能
+//keyBy，window，process
+//keyBy，process。
+//window，process
+
+
+/**
+  * 实时热门商品
+  * 每隔5分钟，输出一次最近1小时内，
+  */
 object HotItems {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.setParallelism(1)
 
-    // 1. 读取数据
-//    val dataStream = env.readTextFile("D:\\Projects\\BigData\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv")
+
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "localhost:9092")
     properties.setProperty("group.id", "consumer-group")
@@ -51,33 +53,53 @@ object HotItems {
       "org.apache.kafka.common.serialization.StringDeserializer")
     properties.setProperty("auto.offset.reset", "latest")
 
+    /**
+      * 从Kafka读数据
+      */
     val dataStream = env.addSource(new FlinkKafkaConsumer[String]("hotitems", new SimpleStringSchema(), properties))
-      .map( data => {
-        val dataArray = data.split(",")
-        UserBehavior( dataArray(0).toLong, dataArray(1).toLong, dataArray(2).toInt, dataArray(3), dataArray(4).toLong )
-      } )
+      //TODO String日志，映射成日志对象
+      .map(data => {
+      val dataArray = data.split(",")
+      UserBehavior(dataArray(0).toLong, dataArray(1).toLong, dataArray(2).toInt, dataArray(3), dataArray(4).toLong)
+    })
+      //TODO 时间戳是升序的，所以用AscendingTimeStamp
       .assignAscendingTimestamps(_.timestamp * 1000L)
 
-    // 2. 开窗聚合数据
+    /**
+      * 按照商品ID分组，开窗，组内聚合求个数，输出出去
+      * 加上时间戳，因为后面的聚合要用
+      */
     val aggregatedStream = dataStream
-      .filter(_.behavior == "pv")    // 过滤出pv行为
-      .keyBy("itemId")    // 按照 itemId分组
-      .timeWindow(Time.hours(1), Time.minutes(5))    // 开滑动窗口
-//      .aggregate(new AverageAgg)
-      .aggregate(new CountAgg(), new WindowResult())    // 窗口聚合操作
+      .filter(_.behavior == "pv")
+      .keyBy("itemId")
+      .timeWindow(Time.hours(1), Time.minutes(5))
+      //      .process()
+      //TODO 如果仅仅是想要聚合的话，用AggravateFunction够了
+      .aggregate(new CountAgg(), new WindowResult())
 
-    // 3. 排序输出 TopN
+    /**
+      * 通过keyBy，把同一窗口的数据聚到同一个组，然后再用process把整个组的数据拿出来，做一下排序，TopN，输出
+      * 输出，其实是需要写入到ES的
+      */
     val resultStream = aggregatedStream
-      .keyBy("windowEnd")    // 按照窗口分组
-      .process( new TopNItems(3) )    // 自定义 process function做排序输出
+      .keyBy("windowEnd") // 按照窗口分组
+      .process(new TopNItems(3)) // 自定义 process function做排序输出
 
+    /**
+      * 输出效果：每隔5分钟，刷新一次的淘宝大屏榜单
+      * windowEnd
+      * top1，XXX商品，点击量
+      * top2，XXX商品，点击量
+      */
     resultStream.print()
     env.execute()
   }
 }
 
-// 自定义预聚合函数
-class CountAgg() extends AggregateFunction[UserBehavior, Long, Long]{
+/**
+  * 统计每个商品Id，对应的日志有多少个的Aggregate聚合函数
+  */
+class CountAgg() extends AggregateFunction[UserBehavior, Long, Long] {
   // 每来一条数据就加 1
   override def add(value: UserBehavior, accumulator: Long): Long = accumulator + 1
 
@@ -87,74 +109,82 @@ class CountAgg() extends AggregateFunction[UserBehavior, Long, Long]{
 
   override def merge(a: Long, b: Long): Long = a + b
 }
-// 另一个例子，计算平均数(数据时间戳的平均值)
-class AverageAgg() extends AggregateFunction[UserBehavior, (Long, Int), Double]{
-  // 每来一条数据，把时间戳叠加在状态1中，把状态2的个数加1
-  override def add(value: UserBehavior, accumulator: (Long, Int)): (Long, Int) =
-    ( accumulator._1 + value.timestamp, accumulator._2 + 1 )
 
-  override def createAccumulator(): (Long, Int) = (0L, 0)
 
-  override def getResult(accumulator: (Long, Int)): Double = accumulator._1 / accumulator._2.toDouble
-
-  override def merge(a: (Long, Int), b: (Long, Int)): (Long, Int) = ( a._1 + b._1, a._2 + b._2 )
-}
-
-class WindowResult() extends WindowFunction[Long, ItemViewCount, Tuple, TimeWindow]{
+/**
+  * 用在aggregate（聚合，window）里面，在聚合函数完成聚合之后，再把数据输入到WindowFunction里面，加上window的时间范围，
+  * 方便后续把同一个窗口的各个商品id的点击量统计数据聚合到一起
+  */
+class WindowResult() extends WindowFunction[Long, ItemViewCount, Tuple, TimeWindow] {
   override def apply(key: Tuple, window: TimeWindow, input: Iterable[Long], out: Collector[ItemViewCount]): Unit = {
     val itemId: Long = key.asInstanceOf[Tuple1[Long]].f0
     val windowEnd: Long = window.getEnd
     val count: Long = input.iterator.next()
-    out.collect( ItemViewCount(itemId, windowEnd, count) )
+    out.collect(ItemViewCount(itemId, windowEnd, count))
   }
 }
 
-// 自定义 process function
-class TopNItems(topSize: Int) extends KeyedProcessFunction[Tuple, ItemViewCount, String]{
-  // 先定义一个列表状态，用来保存当前窗口中所有 item的count聚合结果
+/**
+  * 之前的数据，已经做好了那个窗口，哪个商品ID，对应的点击量是多少的统计了。
+  * 还需要把同一窗口的各个商品点击量的数据聚合到一起，然后把同一窗口的数据给拉取下来，做一下排序，取TopN，然后输出出去就可以了
+  */
+class TopNItems(topSize: Int) extends KeyedProcessFunction[Tuple, ItemViewCount, String] {
+
   private var itemListState: ListState[ItemViewCount] = _
 
+  /**
+    * KeyBy后面的KeyedProcessFunction，不是增量聚合函数。也是没来一条数据就处理一条数据，和aggregate是一样的，所以效率也很高。
+    * 在遍历key中的每一个元素的时候，把都把这个元素给存到状态里面，这样才能把所有数据聚到一起进行以下排序。
+    * 同时，在遍历数据的时候，需要根据数据中的窗口时间，设置上窗口结束的时候的定时器。多个相同时间的定时器，只会注册一个。
+    * 等到窗口时间结束的时候，就会调用定时器的回调用法，回调方法会把存在状态里面的元素数据给排一下序，topN，然后输出
+    * 这样就能实现，每次窗口结束，都进行计算，同时输出本窗口内的所有数据了
+    */
   override def open(parameters: Configuration): Unit = {
     itemListState = getRuntimeContext.getListState(new ListStateDescriptor[ItemViewCount]("item-list", classOf[ItemViewCount]))
   }
 
+
+  /**
+    * key的每个元素调用一次这个方法。如果需要把窗口的所有元素取出来，需要使用状态
+    */
   override def processElement(value: ItemViewCount, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#Context, out: Collector[String]): Unit = {
-    // 每条数据来了之后保存入状态，第一条数据来的时候，注册一个定时器，延迟 1毫秒
     itemListState.add(value)
-    // 可以直接每个数据都注册一个定时器，因为定时器是以时间戳作为id的，同样时间戳的定时器不会重复注册和触发
     ctx.timerService().registerEventTimeTimer(value.windowEnd + 1)
   }
 
-  // 等到watermark超过windowEnd，达到windowEnd+1，说明所有聚合结果都到齐了，排序输出
-  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
-//    val allItemsList: ListBuffer[ItemViewCount] = ListBuffer()
-    import scala.collection.JavaConversions._
-//    for( item <- itemListState.get()){
-//      allItemsList += item
-//    }
-    // 1. 定义一个本地的list，用于提取所有的聚合的值，方便后面做排序
-    val allItemsList = itemListState.get().iterator().toList
-    // 清空list state，释放资源
-    itemListState.clear()
 
-    // 2. 按照count大小进行排序
+  //到达窗口结束时间之后，定时器响，调用回调方法
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Tuple, ItemViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
+
+    import scala.collection.JavaConversions._
+
+    /**
+      * 排序
+      */
+    val allItemsList = itemListState.get().iterator().toList
     val sortedItemsList = allItemsList.sortBy(_.count)(Ordering.Long.reverse).take(topSize)
 
-    // 3. 将排序后列表中的信息，格式化之后输出
+    /**
+      * 把排序的结果输出，可以写成ES的存储逻辑
+      */
     val result: StringBuilder = new StringBuilder()
     result.append("==================================\n")
-    result.append("窗口关闭时间：").append( new Timestamp(timestamp - 1) ).append("\n")
+    result.append("窗口关闭时间：").append(new Timestamp(timestamp - 1)).append("\n")
     // 用一个for循环遍历sortedList，输出前三名的所有信息
-    for( i <- sortedItemsList.indices){
+    for (i <- sortedItemsList.indices) {
       val currentItem = sortedItemsList(i)
-      result.append("No").append( i + 1 ).append(":")
+      result.append("No").append(i + 1).append(":")
         .append(" 商品ID=").append(currentItem.itemId)
         .append(" 浏览量=").append(currentItem.count)
         .append("\n")
     }
-    // 控制显示频率
+
     Thread.sleep(1000L)
-    // 最终输出结果
+
+    //可以把想要输出的数据写到流里面，然后由流进行输出，这里不进行输出
     out.collect(result.toString())
+
+    // 清空list state，释放资源
+    itemListState.clear()
   }
 }
--- NetworkFlowAnalysis/src/main/scala/com/atguigu/networkflow_analysis/HotPageAnalysis.scala
+++ NetworkFlowAnalysis/src/main/scala/com/atguigu/networkflow_analysis/HotPageAnalysis.scala
@@ -31,50 +31,62 @@ case class ApacheLogEvent(ip: String, userId: String, eventTime: Long, method: S
 // 定义中间的聚合结果样例类
 case class UrlViewCount(url: String, windowEnd: Long, count: Long)
 
+/**
+  * 实时热门页面分析
+  * 每隔5分钟输出一次，最近一小时内，用户访问量前N的URL
+  */
 object HotPageAnalysis {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
-    // 读取数据
+
     val resource = getClass.getResource("/apache.log")
-    //    val dataStream = env.readTextFile(resource.getPath)
-    val dataStream = env.socketTextStream("localhost", 7777)
+    val dataStream = env.readTextFile(resource.getPath)
       .map(data => {
         val dataArray = data.split(" ")
-        // 对事件时间进行转换，得到Long的时间戳
         val simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss")
         val timestamp = simpleDateFormat.parse(dataArray(3)).getTime
         ApacheLogEvent(dataArray(0), dataArray(1), timestamp, dataArray(5), dataArray(6))
       })
-      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[ApacheLogEvent](Time.seconds(1)) {
-        override def extractTimestamp(element: ApacheLogEvent): Long = element.eventTime
-      })
 
-    // 开窗聚合
+      //TODO 处理乱序数据的第一重保障：延迟
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[ApacheLogEvent](Time.seconds(1)) {
+      override def extractTimestamp(element: ApacheLogEvent): Long = element.eventTime
+    })
+
+
     val aggStream = dataStream
       .filter(_.method == "GET")
       .keyBy(_.url)
       .timeWindow(Time.minutes(10), Time.seconds(10))
+      //TODO 处理乱序数据的第二种保障，允许迟到数据。window暂时不关闭，每来一条迟到的数据，就把这条迟到数据，合并到之前窗口的计算结果里面去
       .allowedLateness(Time.minutes(1))
+      //TODO 处理乱序数据的第三重保障，晚于最大迟到时间的数据，不要丢弃，而是输出到侧输出流里面，lambda架构，找机会修正之前的实时统计结果，会让统计更加准确
       .sideOutputLateData(new OutputTag[ApacheLogEvent]("late"))
+      //TODO  和之前TopN一样，窗口内聚合，求窗口内每个URL的点击量。加上所属于哪个window
       .aggregate(new UrlCountAgg(), new UrlCountResult())
 
-    // 排序输出
+
     val resultStream = aggStream
+      //TODO 通过keyBy把同一窗口的数据聚合在一起，这样才能对同一窗口的URL进行排序。
       .keyBy(_.windowEnd)
+      //TODO 求相同窗口的数据的TopN，主要是利用process设置定时器，在每次窗口结束的时候，都把统一窗口的数据排一下序，计算一下TopN，然后输出
       .process(new TopNHotUrls(5))
 
     resultStream.print("result")
-    dataStream.print("input")
-    aggStream.print("agg")
+    //TODO 侧输出流的数据，可以用来修正实时计算结果，lambda架构
     aggStream.getSideOutput(new OutputTag[ApacheLogEvent]("late")).print("late")
+
     env.execute("hot page job")
   }
 }
 
-// 自定义预聚合函数
+
+/**
+  * aggregate，对同一个URL的点击量进行统计
+  */
 class UrlCountAgg() extends AggregateFunction[ApacheLogEvent, Long, Long] {
   override def add(value: ApacheLogEvent, accumulator: Long): Long = accumulator + 1
 
@@ -85,47 +97,46 @@ class UrlCountAgg() extends AggregateFunction[ApacheLogEvent, Long, Long] {
   override def merge(a: Long, b: Long): Long = a + b
 }
 
-// 自定义窗口函数，包装成样例类
+/**
+  * aggregate（聚合函数，windowFunction），利用windowFunction，给聚合之后的结果加上窗口的时间戳，这样后续就能区分这条数据是属于哪个窗口的了，就能把同一窗口的数据给聚合起来，然后进行窗口内的排序。否则无法进行窗口内的排序
+  */
 class UrlCountResult() extends WindowFunction[Long, UrlViewCount, String, TimeWindow] {
   override def apply(key: String, window: TimeWindow, input: Iterable[Long], out: Collector[UrlViewCount]): Unit = {
     out.collect(UrlViewCount(key, window.getEnd, input.iterator.next()))
   }
 }
 
-// 自定义实现Process Function，先保存列表状态，然后定时排序输出
+
+/**
+  * keyBy之后，同一窗口的URL的点击量统计结果就聚合在一起了，需要通过定时器，在每次窗口结束的时候，都调用定时器的回调方法，把同一窗口的数据拉取下来，然后进行排序，输出
+  */
 class TopNHotUrls(topSize: Int) extends KeyedProcessFunction[Long, UrlViewCount, String] {
-  // 先定义一个list状态，用来保存所有的聚合结果
-  //  lazy val urlListState: ListState[UrlViewCount] = getRuntimeContext.getListState(new ListStateDescriptor[UrlViewCount]("url-list", classOf[UrlViewCount]))
-  // 改进：用 MapState (url, count)来保存聚合结果，用于迟到数据更新聚合结果时去重
+  //TODO 如果不考虑迟到的那部分数据的话，就把每个窗口的数据，放在List里面就可以，每次窗口结束，都触发定时器的回调方法求TopN就可以了。如果考虑迟到数据的话，因为每条迟到数据都会触发窗口后续的所有计算，而且所有计算，都是在原来计算的基础上进行计算的。所以，肯定会产生数据重复的问题，比如原来整个窗口，在进行完聚合之后，会输出一批URL的点击量统计数据。但是来了一条迟到数据，那就会重新把这个窗口内的每个URL的点击量给统计一遍，然后再重新把这个窗口的URL点击量的统计数据输出一遍。这样后面的keyBy之类的计算，就会收集到同一个窗口的两批数据。如果还是正常的keyBy，然后统计的话，就错了，比如同一个URL的点击量可能出现两次，或者多次。解决这个问题的办法，就是不用List了，换成用Map，每次迟到数据来了，会把同一个窗口的URL的点击量数据重新发一遍，然后经过keyBy之后，到达process，process会用新来的一批数据，覆盖掉旧的数据，然后重新触发定时器的回调方法，进行TopN的统计。
   lazy val urlMapState: MapState[String, Long] = getRuntimeContext.getMapState(new MapStateDescriptor[String, Long]("url-map", classOf[String], classOf[Long]))
 
   override def processElement(value: UrlViewCount, ctx: KeyedProcessFunction[Long, UrlViewCount, String]#Context, out: Collector[String]): Unit = {
-    // 每条数据直接放入list state
+    //TODO 来一条数据（URL，次数），就把这个存到List里面。经过改进之后，就是存到Map里面
     urlMapState.put(value.url, value.count)
-    // 定义一个 windowEnd + 1 的定时器
+    //TODO 设置定时器。不是为数据设置定时器，而是借用数据里面的窗口结束时间戳，给窗口设置定时器。等到窗口结束的时候，进行统计计算和输出
     ctx.timerService().registerEventTimeTimer(value.windowEnd + 1)
   }
 
-  // 定时器触发时，watermark涨过了windowEnd，所以所有聚合结果都到期了
+
   override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, UrlViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
-    // 从状态获取所有的聚合结果
-    //    val allUrlsCount: ListBuffer[UrlViewCount] = new ListBuffer[UrlViewCount]()
-    //    val iter = urlListState.get().iterator()
-    //    while(iter.hasNext)
-    //      allUrlsCount += iter.next()
+
     val allUrlsCount: ListBuffer[(String, Long)] = new ListBuffer[(String, Long)]()
     val iter = urlMapState.entries().iterator()
     while (iter.hasNext) {
       val entry = iter.next()
+      //TODO 把Map状态中存的URL统计数据，提取出来，方便排序。不然在Map里面不方便排序
       allUrlsCount += ((entry.getKey, entry.getValue))
     }
 
-//    urlListState.clear()
 
-    // 排序输出
+    //TODO 排序，取TopN
     val sortedUrlsCount = allUrlsCount.sortWith(_._2 > _._2).take(topSize)
 
-    // 格式化输出
+    //TODO 拼接字符串，把字符串传给流，让流进行输出
     val result: StringBuilder = new StringBuilder()
     result.append("==================================\n")
     result.append("窗口关闭时间：").append(new Timestamp(timestamp - 1)).append("\n")
@@ -137,9 +148,9 @@ class TopNHotUrls(topSize: Int) extends KeyedProcessFunction[Long, UrlViewCount,
         .append(" 访问量=").append(currentUrl._2)
         .append("\n")
     }
-    // 控制显示频率
+
     Thread.sleep(1000L)
-    // 最终输出结果
+    //TODO 把拼接好的结果字符串，传到流中，让流输出数据，而不是在这里输出数据。如果是ES的话，可以在这里输出数据
     out.collect(result.toString())
   }
 }
