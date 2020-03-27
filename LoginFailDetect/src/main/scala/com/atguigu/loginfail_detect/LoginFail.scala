15cf66c77808b6ee58291ba67d2903402aeb4970
CSer2294365315 <2294365315@qq.com>
2020-03-27 16:08:07 +0800

    3月27日 commit

--- LoginFailDetect/src/main/scala/com/atguigu/loginfail_detect/LoginFail.scala
+++ LoginFailDetect/src/main/scala/com/atguigu/loginfail_detect/LoginFail.scala
@@ -59,6 +59,7 @@ object LoginFail {
 }
 
 
+
 class LoginWarning(maxFailTimes: Int) extends KeyedProcessFunction[Long, LoginEvent, Warning] {
   //TODO 保存上次是否登录失败的list状态
   lazy val loginFailListState: ListState[LoginEvent] = getRuntimeContext.getListState(
